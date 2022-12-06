f9e7bc4a5e39a7c9e97dc148cfb92daffe9ff11d
boumba100 <noah.bouma@gmail.com>
2022-12-06 07:10:43 -0500

    Added router agent build scripts

---
+++ build-router-agent.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Test token package
+cargo test --manifest-path veronymous-token/Cargo.toml
+cargo test --manifest-path veronymous-token/crypto/Cargo.toml
+cargo test --manifest-path veronymous-token/crypto/bb-signatures/Cargo.toml
+cargo test --manifest-path veronymous-token/crypto/commitments/Cargo.toml
+cargo test --manifest-path veronymous-token/crypto/common/Cargo.toml
+cargo test --manifest-path veronymous-token/crypto/common/Cargo.toml
+
+# Test connection package
+cargo test --manifest-path veronymous-connection/Cargo.toml
+
+# Test the router agent
+cargo test --manifest-path veronymous-router-agent/Cargo.toml
+
+# Build the binary
+cargo build --release --manifest-path veronymous-router-agent/Cargo.toml
+
+# Build the docker image
+docker build -t veronymous-router-agent -f veronymous-router-agent.Dockerfile ./target/release
